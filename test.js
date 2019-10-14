9c06cddf3d5daf1dd8d59875c4814a3935c5ceeb
Sebastian Lague <sebastlague@gmail.com>
2019-10-14 19:58:53 +0200

    p5 test

---
+++ test.js
@@ -0,0 +1,15 @@
+
+function setup() {
+	createCanvas(1280, 720);
+
+}
+
+function draw() {
+	background(30);
+
+	fill(255)
+	ellipse(mouseX,mouseY,50,50)
+
+	let display = touches.length + ' touches';
+  	text(display, 5, 10);
+}
