613bc3a4415072c4c13aca467119aa722a73f58c
Sebastian Lague <sebastlague@gmail.com>
2019-10-15 15:07:12 +0200

    separated css and js to own files

--- test.js
+++ test.js
@@ -1,15 +1,5 @@
-
-function setup() {
-	createCanvas(1280, 720);
-
-}
-
-function draw() {
-	background(80,30,30);
-
-	fill(255)
-	ellipse(mouseX,mouseY,50,50)
-
-	let display = touches.length + ' touches';
-  	text(display, 5, 10);
+var x = 0;
+function myFunction() {
+	x+=1;
+	document.getElementById("demo").innerHTML = "Hello World: " + x;
 }
