a6f4bf137bf11b96977a2777b30a2695678b2e83
SirHall <sirhallstein@gmail.com>
2019-12-16 20:51:40 +1000

    Split off super string in data.js into smaller files allowing more easy editing of the category's contents

---
+++ ideagenerator/categories/cat_goal_prefix.js
@@ -0,0 +1,8 @@
+//----------------- GOALS (you play as a character/group of characters trying to...) -----------------
+
+data['goal_prefix'] = `
+[trying, attempting] to
+who [desperately,] (wants,want) to
+who will stop at nothing to
+who (needs,need) to
+`
